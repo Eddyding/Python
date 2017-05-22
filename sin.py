98f3b6fd08eff3768bea99fa3ce32d424b404e1e
Eddyding <920398694@qq.com>
2017-05-22 23:09:12 +0800

    python sin plot

---
+++ sin.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as pt
+
+
+x = np.arange(0,360)
+
+y = np.sin(x * np.pi/180.0)
+
+
+pt.plot(x,y,label="sin(x)")
+
+pt.xlim(0,360)
+pt.ylim(-1.1,1.1)
+
+pt.title("SIN function")
+pt.legend()
+
+pt.savefig("figure/1.png",dpi=72)
+
+pt.grid()
+pt.show()
+
+
+
---
+++ test1.py
@@ -0,0 +1,6 @@
+age = int(input("age is:"))
+if age >= 18:
+    print("Adult")
+else:
+    diff = str(18-age)
+    print("adult diff " + diff+"sui")
