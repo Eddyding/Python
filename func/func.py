5a0b3813dcd58cd320e7c5a38ac2c6b7c8737fc8
Eddyding <920398694@qq.com>
2017-06-16 19:18:02 +0800

    func

--- func/func.py
+++ func/func.py
@@ -14,36 +14,38 @@ chr(n):取得第n个ASCII码的字符
 dir(x):检查x对象可以使用的方法.
 
 
-divmod(a,b)
-enumerate(x)
+divmod(a,b): 返回a/b的商和余数,以tuple的形式返回
+enumerate(x):用枚举的形式,把变量x中的索引值和值 取出来,组合成tuple,而x必须像是list,dict这一类具有迭代特性的变量
 
-eval(e)
-float(n)
+eval(e):求字符串类型的表达式e的值.
+float(n):将变量n转换成浮点型.
 
-format()
+format():字符串格式化,输出映像
 
-frozenset()
+frozenset(): 用来创建不能被修改的集合变量
 
-help(cmd)
-hex(n)
-id(x)
-input(msg)
+help(cmd):查询任一指令或 函数的用法
+hex(n):把数值n转换成16进制
+id(x):取得变量x的内存地址
+input(msg):显示出信息msg,并要求用户输入数据
 
-int(a)
-len(a)
-max(a)
-min(a)
-oct(n)
-open()
-pow(x,y)
-print()
+int(a):把变量a转换成整型数.
+len(a):计算变量a的长度.但a必须是可以计算长度的类型.
+max(a):返回变量a中最大值的元素
+min(a):返回变量a中最小值的元素
+oct(n):把数值n转换成8进制
+open():打开文件
+pow(x,y):x的y次方
+print():
 
 range(a,b,c) #[a..b-1],间隔为c
-round(n)
-sorted(a)
-str(n)
-sum(a)
-type(x)
+round(n):数值n四舍5入,取整数
+sorted(a):把a的元素排序
+str(n):把变量n转换成字符串类型
+sum(a):计算a中元素的总和
+type(x):返回x的类型
+
+
 '''
 
 
