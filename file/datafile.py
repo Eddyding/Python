cd98be7b64b3a96aa1667501821e330f4d0fb6f8
Eddyding <920398694@qq.com>
2017-06-18 09:29:07 +0800

    datafile

---
+++ file/datafile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# _*_ coding: UTF-8 _*_
+
+#Python 3
+
+'''
+fp = open("文件名",”文件打开模式“)
+
+文件打开模式: r,w ,a
+
+在大部分的应用中，都是以文本文件的方式来存取文件的内容，也就是说，以 字符串的类型来  查看文件的内容。
+
+
+read()  : 一次性把 所有内容 以字符串的形式读入变量中。
+readline(): 一次只读取文件的一行内容。
+readlines(): 把每一行拆开 放在每一个不同的字符串变量中，并以列表的方式汇集在一起。
+
+
+'''
+
+
+import this
+
+print("------------")
+fp = open("zop.txt","r")
+
+zops = fp.readlines()
+
+fp.close()
+
+
+i =1
+print("The zen of python")
+for zen in zops:
+    print("Zen {}:{}".format(i,zen),end="")
+    i +=1
--- file/ossystemme.py
+++ file/ossystemme.py
@@ -16,8 +16,33 @@ cmd: 可以执行os命令的字符串
 
 import os
 
+import commands
+
+
 os.system("ls -al .")
 os.system("cp file2.py file3.py")
 os.system("cat ossystemme.py")
 
-os.system("clear")
+#os.system("clear")
+
+print(commands.getstatusoutput("ls -al ."))
+
+
+'''
+    
+shutil :高级的目录和文件操作模块
+
+copyfile(s,d)  把文件s 复制为 文件d(仅复制文件内容，不含属性)
+copy(s,d)   把文件s 复制为 文件d(含文件的权限属性)
+
+copy2(s,d)  把文件s 复制为 文件d(含所有的文件属性)
+copytree(s,d)  把整个目录s(包含里面所有的内容)复制一份到d
+rmtree(p)   删除p这个目录以及里面所有的内容
+move(s,d)  把s这个目录或 文件  搬移 到 d
+
+
+删除文件：os.remove(file) 
+
+删除一个空目录： os.rmdir(dir)
+
+'''
