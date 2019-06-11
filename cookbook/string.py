dcabed33dd33d09f70d115ae070dc8b324da6808
profding-bh <--global>
2019-06-11 12:30:32 +0800

    string.py

---
+++ cookbook/string.py
@@ -0,0 +1,18 @@
+>>> line='asdf fjdk; afed, fjek,asdf,     foo'
+>>> import re
+>>> re.split(r'[;,\s]\s*', line)
+['asdf', 'fjdk', 'afed', 'fjek', 'asdf', 'foo']
+>>> fields = re.split(r'(;|,|\s)\s*', line)
+>>> fields
+['asdf', ' ', 'fjdk', ';', 'afed', ',', 'fjek', ',', 'asdf', ',', 'foo']
+>>> values = fields[::2]
+>>> delimiters=fields[1::2]+['']
+>>> values
+['asdf', 'fjdk', 'afed', 'fjek', 'asdf', 'foo']
+>>> delimiters
+[' ', ';', ',', ',', ',', '']
+>>> # Reform the line using the same delimiters
+>>> ''.join(v+d for v,d in zip(values,delimiters))
+'asdf fjdk;afed,fjek,asdf,foo'
+>>> re.split(r'(?:,|;|\s)\s*', line)
+['asdf', 'fjdk', 'afed', 'fjek', 'asdf', 'foo']
